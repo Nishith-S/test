abf85f8d0439dfdf084fcc96e5331d8d1525d1a9
Nishith-S <solankinishith910@gmail.com>
2024-07-26 14:19:48 +0530

    Just added a navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_test/pages/login.dart';
 
 void main() {
   runApp(const MyApp());
@@ -116,7 +117,9 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.of(context).push(MaterialPageRoute(builder: (context) => Login()));
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
